4ecace102d7c343660b6ac119bc3d6fc1586991e
Crowdbotics <team@crowdbotics.com>
2023-07-18 19:28:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "myriad-motivations-42262",
+  url: "https://myriad-motivations-42262.botics.co",
+  api: "https://myriad-motivations-42262.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
